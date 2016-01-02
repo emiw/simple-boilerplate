6d3d027b0c993bfc628ef415c89886e6ef8c32cd
Ari Porad <ari@ariporad.com>
2016-01-01 16:37:44 -0800

    chore: add remaning npm scripts

--- test/test.js
+++ test/test.js
@@ -1,8 +1,8 @@
-/* (c) 2015 Ari Porad (@ariporad) <http://ariporad.com>. License: ariporad.mit-license.org */
+/* (c) 2016 Ari Porad (@ariporad) <http://ariporad.com>. License: ariporad.mit-license.org */
 import test from 'ava';
+import doSomething from '../';
 
 test(t => {
   t.ok(true);
+  doSomething();
 });
-
-
